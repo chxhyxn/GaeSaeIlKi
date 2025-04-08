1ef3d08247ec350c766ee88d9b4892359fddec47
chxhyxn <chxhyxn@gmail.com>
2025-04-09 08:48:48 +0900

    Initial Commit

---
+++ GaeSaeIlKi/GaeSaeIlKiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaeSaeIlKiApp.swift
+//  GaeSaeIlKi
+//
+//  Created by Sean Cho on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GaeSaeIlKiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
